3542488637edb42c315fd1aab4d77b3fdc9cb3bb
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-06-26 19:36:36 +0530

    Android build script fixes (#4)
    * Simplified android build scripts. Now single script will build for all the architecture.
    Removed dependencies building scripts and instructions.
    Libnullpay uses same mechanism for building as libindy
    Build scripts downloads prebuilt binaries while building.
    
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>
    
    * Moved the logic of creating the directory in android environment to another function
    
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>
    
    * Now libindy panics when it cannot find path in EXTERNAL_STORAGE variable on android.
    
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

--- libindy/src/utils/environment.rs
+++ libindy/src/utils/environment.rs
@@ -16,11 +16,29 @@ impl EnvironmentUtils {
         path.push(indy_client_dir);
 
         if cfg!(target_os = "android"){
-            let android_dir = env::var("EXTERNAL_STORAGE").unwrap() + "/.indy_client";
-            fs::create_dir_all(&android_dir).unwrap();
-            path = PathBuf::from(android_dir);
+            EnvironmentUtils::android_create_indy_client_dir();
+            path = EnvironmentUtils::android_indy_client_dir_path();
         }
         path
+    }
+
+    pub fn android_indy_client_dir_path() -> PathBuf{
+        let external_storage= env::var("EXTERNAL_STORAGE");
+        let android_dir :String;
+        match external_storage {
+            Ok (val) => android_dir = val + "/.indy_client",
+            Err(err) => {
+                error!("Unable to find EXTERNAL_STORAGE android environment variable which holds the location to external storage file path");
+                panic!("Failed to find external storage path {:?}", err)
+            }
+        }
+
+        PathBuf::from(android_dir)
+    }
+
+    pub fn android_create_indy_client_dir(){
+        //Creates directory only if it is not present.
+        fs::create_dir_all(EnvironmentUtils::android_indy_client_dir_path().as_path()).unwrap();
     }
 
     pub fn wallet_home_path() -> PathBuf {
